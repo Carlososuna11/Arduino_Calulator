f01e846ea161f4d3a51b860039a4c0fa3d971af7
Carlososuna11 <37004057+Carlososuna11@users.noreply.github.com>
2021-01-24 09:56:30 -0400

    Create calculadora.ino

---
+++ calculadora.ino
@@ -0,0 +1,98 @@
+#include <LiquidCrystal.h>
+#include <Keypad.h>
+
+LiquidCrystal lcd(0,1,2,3,4,5);
+const byte ROWS = 4;
+const byte COLS = 4;
+
+char keys [ROWS] [COLS] = {
+    {'1','2','3','+'},
+    {'4','5','6','-'},
+    {'7','8','9','*'},
+    {'C','0','=','/'}
+};
+
+byte rowPins[ROWS] = {A0,A1,11,10};
+byte colPins[COLS] = {9,8,7,6};
+
+Keypad Teclado = Keypad(makeKeymap(keys),rowPins,colPins,ROWS,COLS);
+
+boolean presentValue = false;
+boolean next = false;
+boolean final = false;
+String num1, num2;
+int answer;
+char op;
+
+void setup(){
+    lcd.begin(16,2);
+    lcd.setCursor(0,0);
+    lcd.print("   Circuitos   ");
+    lcd.setCursor(0,1);
+    lcd.print("  Calculadora  ");
+    delay(3000);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("   Hecho Por   ");
+    lcd.setCursor(0,1);
+    lcd.print("  Carlos Osuna ");
+    delay(3000);
+    lcd.clear();
+} 
+
+void loop(){
+    char key = Teclado.getKey();
+    if (key){
+
+        if (key != NO_KEY && (key=='1'||key=='2'||key=='3'||key=='4'||key=='5'||key=='6'||key=='7'||key=='8'||key=='9'||key=='0')){
+            if (presentValue != true){
+                num1 = num1 + key; 
+                int numLength = num1.length();
+                lcd.setCursor(15-numLength,0);
+                lcd.print(num1);
+            }else{
+                num2 = num2 + key;
+                int numLength = num2.length();
+                lcd.setCursor(15-numLength,1);
+                lcd.print(num2);
+                final = true;
+            }
+        }
+        else if (presentValue == false && key != NO_KEY && (key =='/'||key=='*'||key=='+'||key == '-')){
+            if (presentValue == false){
+                presentValue = true;
+                op=key;
+                lcd.setCursor(15,0);
+                lcd.print(op);
+            }
+        }
+        else if (final== true && key != NO_KEY && key=='='){
+            if (op =='+'){
+                answer = num1.toInt() + num2.toInt();
+            }
+            else if (op =='-'){
+                answer = num1.toInt() - num2.toInt();
+            }
+            else if (op =='*'){
+                answer = num1.toInt() * num2.toInt();
+            }
+            else if (op =='/'){
+                answer = num1.toInt() / num2.toInt();
+            }
+            lcd.clear();
+            lcd.setCursor(15,0);
+            lcd.autoscroll();
+            lcd.print(answer);
+            lcd.noAutoscroll();
+        }
+        else if (key != NO_KEY && key == 'C'){
+            lcd.clear();
+            presentValue = false;
+            final = false; 
+            num1 =' ';
+            num2 =' ';
+            answer = 0;
+            op = ' ';
+        }
+    }
+}
